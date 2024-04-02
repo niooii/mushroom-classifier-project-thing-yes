8b391473678ea3ff5d83b5d51f06947fefee22ef
niooii <wangh3@bxscience.edu>
2024-04-02 14:06:01 -0400

    baseleflsefes

--- src/model.rs
+++ src/model.rs
@@ -2,7 +2,7 @@ use std::{collections::{HashMap, HashSet}, hash::Hash};
 
 use itertools::Itertools;
 
-use crate::{csv_parser::CsvFile, tree::Tree};
+use crate::{csv_parser::CsvFile, traits::CsvParsable, tree::Tree};
 
 pub fn calc_entropy<T>(labels: &Vec<T>) -> f32
 where T: PartialEq + Eq + Hash + Clone + Ord
@@ -18,6 +18,40 @@ where T: PartialEq + Eq + Hash + Clone + Ord
         label_count as f32 / total_labels as f32 * (1_f32 / (label_count as f32 / total_labels as f32)).log2()
     }).sum()
 } 
+
+pub struct FeatureIdxMap<T>
+where T: CsvParsable + PartialEq
+{
+    features: Vec<T>
+}
+
+impl<T> FeatureIdxMap<T> 
+where T: CsvParsable + PartialEq
+{
+    pub fn new() -> Self {
+        Self {
+            features: Vec::new()
+        }
+    }
+
+    // returns the feature index
+    pub fn add(&mut self, feature: T) -> usize {
+        if self.features.contains(&feature) {
+            panic!("feature is already in here DUMBASS");
+        }
+
+        self.features.push(feature);
+        self.features.len() - 1
+    }
+
+    pub fn get_feature(&self, idx: usize) -> Option<&T> {
+        self.features.get(idx)
+    }
+
+    pub fn get_idx(&self, feature: &T) -> Option<usize> {
+        self.features.iter().find(|e| e == feature)
+    }
+}
 
 #[derive(PartialEq)]
 pub enum DesicionTreeNode {
@@ -43,6 +77,8 @@ impl DesicionTree {
     pub fn new(csv_file: &CsvFile) -> Self {
 
         let all_columns = csv_file.read_all_columns::<char>();
+        // (info gain, feature_index, split_on_idx)
+        let mut info_gain_vec = Vec::<(f32, usize, u8)>::new();
 
         for column in &all_columns {
             let entropy = calc_entropy(&column.data);
@@ -74,7 +110,7 @@ impl DesicionTree {
             println!("distribution of unique labels: {:?}", unique_label_count_byte_vals);
 
             // loop through bytes as u8 to find the highest information gain
-            let max_info_gain = unique_label_count_byte_vals.iter().clone()
+            let info_gain_and_split_idx = unique_label_count_byte_vals.iter().clone()
                 .map(|(byte, _)| *byte).sorted()
                 .skip(1)
                 .map(|byte| {
@@ -103,12 +139,18 @@ impl DesicionTree {
             ).max_by(|a, b| a.partial_cmp(b).unwrap())
             .unwrap();
 
-            let best_info_gain = max_info_gain.0;
-            let best_point_of_split = max_info_gain.1;
+            let best_info_gain = info_gain_and_split_idx.0;
+            let best_point_of_split = info_gain_and_split_idx.1;
             println!("best point of split: {best_point_of_split}");
             println!("max information gain: {best_info_gain}");
             println!();
+
+            info_gain_vec.push((best_info_gain, 0/*TODO!*/, best_point_of_split));
+            
         }
+
+        info_gain_vec.sort_by(|a, b| a.0.total_cmp(&b.0));
+        println!("info gain vec: {:?}", info_gain_vec);
 
         Self {
             tree: Tree::new(unimplemented!())
